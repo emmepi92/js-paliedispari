105d713d07b916e4581a383579ba9707832e0f67
emmepi92 <mariapiacoccioli@live.it>
2021-06-09 16:11:21 +0200

    forse finito

--- js/script.js
+++ js/script.js
@@ -32,5 +32,50 @@ outputHtml.innerHTML = risultato;
 
 
 
+//-------------------------------------------------
+
+var userPariOrDispari = prompt("Scegli fra pari o dispare, scrivendo 'pari' o 'dispari");
+var userNum = parseInt(prompt("Inserisci un numero tra 1 e 5"));
+
+function getRandonFrom1to5 () {
+    var num = Math.floor(Math.random() * 5 + 1);
+    return num;
+}
+
+var pcNum = getRandonFrom1to5();
+
+
+function isSumPari (num1, num2) {
+    var sum = num1 + num2;
+    var pari = false
+    
+    if (sum % 2 === 0) {
+        pari = true;
+    } // else (dispari) e quindi pari resta false
+    return pari  
+}
+result2 = '';
+
+var sum = isSumPari( userNum, pcNum);
+
+if ( (sum === true && userPariOrDispari === 'pari') || (sum === false && userPariOrDispari === 'dispari')) {
+    result2= 'Bravo hai vinto contro il pc';
+} else {
+    result2 = 'Peccato hai perso contro il pc';
+}
+
+outputHtml.innerHTML += '<br/>' + result2;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
