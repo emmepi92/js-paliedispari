77cd7f2cb5cb7f679f1c547074657fa4ab3fb51f
emmepi92 <mariapiacoccioli@live.it>
2021-06-09 16:17:02 +0200

    sclero

--- js/script.js
+++ js/script.js
@@ -33,17 +33,10 @@ outputHtml.innerHTML = risultato;
 
 
 //-------------------------------------------------
-
-var userPariOrDispari = prompt("Scegli fra pari o dispare, scrivendo 'pari' o 'dispari");
-var userNum = parseInt(prompt("Inserisci un numero tra 1 e 5"));
-
 function getRandonFrom1to5 () {
     var num = Math.floor(Math.random() * 5 + 1);
     return num;
 }
-
-var pcNum = getRandonFrom1to5();
-
 
 function isSumPari (num1, num2) {
     var sum = num1 + num2;
@@ -54,7 +47,12 @@ function isSumPari (num1, num2) {
     } // else (dispari) e quindi pari resta false
     return pari  
 }
+
+var userPariOrDispari = prompt("Scegli fra pari o dispare, scrivendo 'pari' o 'dispari");
+var userNum = parseInt(prompt("Inserisci un numero tra 1 e 5"));
 result2 = '';
+
+var pcNum = getRandonFrom1to5();
 
 var sum = isSumPari( userNum, pcNum);
 
@@ -63,6 +61,10 @@ if ( (sum === true && userPariOrDispari === 'pari') || (sum === false && userPar
 } else {
     result2 = 'Peccato hai perso contro il pc';
 }
+
+// mi serve per capire bene il debug
+outputHtml.innerHTML += "<br/>Numero dell'utente è " + userNum + " <br/> Il numero del pc è " + pcNum +
+                        "<br/> La loro somma è " + (userNum + pcNum) + "<br/>L'utente ha scommesso su " + userPariOrDispari;
 
 outputHtml.innerHTML += '<br/>' + result2;
 
